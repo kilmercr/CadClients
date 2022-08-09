7e42822210a0efd3b6d1d2aa32342dcb547daa2f
Kilmer Cruz de Souza <kilmer.souza@gmail.com>
2022-08-08 21:33:51 -0300

    Otimizando código.

--- src/main/java/com/myprojects/cadclients/dtos/ClientDto.java
+++ src/main/java/com/myprojects/cadclients/dtos/ClientDto.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.br.CPF;
