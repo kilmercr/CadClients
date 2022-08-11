ea79d998a130419b47ab47267dbf897160dfde24
Kilmer Cruz de Souza <kilmer.souza@gmail.com>
2022-08-11 17:51:55 -0300

    Melhoria no teste unitário do ClientService.

--- src/test/java/com/myprojects/cadclients/service/ClientServiceTest.java
+++ src/test/java/com/myprojects/cadclients/service/ClientServiceTest.java
@@ -12,7 +12,6 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
